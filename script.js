8f7f58e28a08f6acbed61a37e715d1e79262068e
LauraLopezx5 <99838714+LauraLopezx5@users.noreply.github.com>
2022-05-10 21:35:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,42 @@
 
-
-class Producto {
-    constructor(nombre, marca, precio, stock){
+class cuenta {
+    constructor(nombre,contraseña){
         this.nombre = nombre
-        this.marca = marca
-        this.precio = precio
-        this.stock = stock
-    }
+        this.contraseña = contraseña
+    }  
 }
 
-function verProductos(){
-    PRODUCTOS.forEach((prod) => {
-        console.log(prod)
-    })
-}
+let cuenta1 = new cuenta("Laurax5",1234);
+let cuenta2 = new cuenta("LuisSC",4815)
+let cuenta3 = new cuenta ("Maria",3335)
 
-function buscarProducto(){
-    let busqueda = prompt("Ingrese producto")
-    PRODUCTOS.forEach((prod)=>{
-        if(prod.nombre==busqueda){
-            console.log("vas a comprar",prod.nombre, prod.marca,"precio $",prod.precio,"stock", prod.stock)
-            prod.stock-=1
-            console.log("muchas gracias")
+let cuentas = [cuenta1,cuenta2,cuenta3]
+
+let boton = document.querySelector("#boton")
+let usuario = document.querySelector("#usuario")
+let contraseña = document.querySelector("#contraseña")
+let mensaje = document.querySelector ("#mensaje")
+let resultadoBusqueda = false
+
+
+
+function buscarCuenta(nombre,contraseña){
+    cuentas.forEach(cuenta =>{
+        if(cuenta.nombre==nombre&&cuenta.contraseña==contraseña){
+            resultadoBusqueda = true
         }
     })
 }
 
-const Producto1 = new Producto("zapatilla","adidas",7500,7)
-const Producto2 = new Producto("campera","nike",11000,5)
-const Producto3 = new Producto("remera", "puma",3000,8)
 
-let PRODUCTOS =  [Producto1,Producto2,Producto3]
-
-let salir = false
-
-do{
-    let opcion = parseInt(prompt("seleccione una opcion. 1 Ver productos 2 Comprar 3 Salir"))
-    switch(opcion){
-        case 1:
-            verProductos()
-            break
-        case 2:
-            buscarProducto()
-            
-            break
-        case 3:
-            salir=true
-            break
+boton.addEventListener("click",()=>{
+    buscarCuenta(usuario.value,contraseña.value)
+    if(resultadoBusqueda==true){
+        mensaje.innerHTML = "Hola " + usuario.value + "!"
+    }else{
+        mensaje.innerHTML = "Usuario o contraseña incorrecta"
     }
-
-}while(salir == false)
-
-console.log(PRODUCTOS)
-console.log("Hasta Luego")
-
-
-
+    usuario.value = ""
+    contraseña.value = ""
+})
 
